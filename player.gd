a02002fbbd86d954276edd02f7caa2b0bd336e9b
becen02 <121971428+becen02@users.noreply.github.com>
2023-12-14 14:42:10 -0800

    Initial commit

--- player.gd
+++ player.gd
@@ -1,13 +1,16 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 @export var speed = 400
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_up"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.x += 1
+	if velocity.length > 0:
+		
